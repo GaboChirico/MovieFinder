33e0b5bb7e9dc25adca17b227546f12119648864
Gabriel Chirico <97834322+GaboChirico@users.noreply.github.com>
2023-05-08 17:42:35 +0200

    add str to dict

--- src/models.py
+++ src/models.py
@@ -1,13 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Dict
 
 
 @dataclass
 class Query:
     value: str
+    region: str
 
 
 @dataclass
 class Result:
-    title: str
-    platforms: Dict[str, str]
+    title: str = field(default_factory=str)
+    platforms: Dict[str, str] = field(default_factory=dict)
+
+    def __str__(self):
+        return str(self.__dict__)
